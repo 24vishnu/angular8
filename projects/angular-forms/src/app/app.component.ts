cf1c82712eec060bdd28d8edb0e97fd625ea1758
vishnu24 <vishnu23kumar@gmail.com>
2020-05-13 15:45:36 +0530

    setValue_and_patchValue

--- projects/angular-forms/src/app/app.component.ts
+++ projects/angular-forms/src/app/app.component.ts
@@ -10,7 +10,25 @@ export class AppComponent {
   @ViewChild('f', {static: true}) signUpForm: NgForm;
   defaultQuestion = 'place';
   answer = '';
+  genders = ['male', 'female'];
 
+  suggestUserName() {
+    const suggestName = 'superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestName,
+    //     email: ''
+    //   },
+    //   secret: 'sport',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestName
+      }
+    });
+  }
   onSubmit() {
     console.log(this.signUpForm);
   }
