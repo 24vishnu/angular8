e239b478bb254534302366ca37cdc4595cb6db0e
vishnu24 <vishnu23kumar@gmail.com>
2020-05-13 17:20:31 +0530

    _start_reactiveForms_create_and_submit

--- projects/angular-forms/src/app/app.component.ts
+++ projects/angular-forms/src/app/app.component.ts
@@ -1,50 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('f', {static: true}) signUpForm: NgForm;
-  defaultQuestion = 'place';
-  answer = '';
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    screreQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
 
-  suggestUserName() {
-    const suggestName = 'superuser';
-    // this.signUpForm.setValue({
-    //   userData: {
-    //     username: suggestName,
-    //     email: ''
-    //   },
-    //   secret: 'sport',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-    this.signUpForm.form.patchValue({
-      userData: {
-        username: suggestName
-      }
+  // Rective form/
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male')
     });
   }
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.signUpForm.value.userData.username;
-    this.user.email = this.signUpForm.value.userData.email;
-    this.user.screreQuestion = this.signUpForm.value.secret;
-    this.user.answer = this.signUpForm.value.questionAnswer;
-    this.user.gender = this.signUpForm.value.gender;
 
-    this.signUpForm.reset();
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
